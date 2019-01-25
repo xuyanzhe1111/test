f75f4aa812a67be25911d791f20356793cdeab9c
xuyanzhe <648485080@qq.com>
2019-01-25 16:06:17 +0800

    new

--- hello_java/src/hello_java/Hello.java
+++ hello_java/src/hello_java/Hello.java
@@ -2,7 +2,8 @@ package hello_java;
 import java.util.*;
 public class Hello {
 
-	/**a new  main class*/ 
+	/**a new  main class*/
+	/**a new  main class*/
 	public static void main(String args[] )
 	{
 		System.getProperties().list(System.out);
