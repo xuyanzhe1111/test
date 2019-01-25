7f2f1f8ac8ae920c671528adb01bc81aea17dd28
xuyanzhe <648485080@qq.com>
2019-01-25 10:04:59 +0800

    test

---
+++ hello_java/src/hello_java/Hello.java
@@ -0,0 +1,12 @@
+package hello_java;
+import java.util.*;
+public class Hello {
+
+	/**a new  main class*/ 
+	public static void main(String args[] )
+	{
+		System.getProperties().list(System.out);
+		System.out.println(System.getProperty("user name"));
+		System.out.println(System.getProperty("java.library.path")+1);
+	}
+}
